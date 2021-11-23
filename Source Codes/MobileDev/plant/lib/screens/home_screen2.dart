0118df042b747e447fb165cdb1be20d16c1458da
LalinduWenasara <lalinduwenu@gmail.com>
2021-11-23 11:43:59 +0530

    added dummy containers for services

--- Source Codes/MobileDev/plant/lib/screens/home_screen2.dart
+++ Source Codes/MobileDev/plant/lib/screens/home_screen2.dart
@@ -8,7 +8,6 @@ import 'package:plant/screens/select_plant_screen.dart';
 
 import '../reusable.dart';
 
-
 class Home5 extends StatefulWidget {
   static const String id = 'home5';
 
@@ -18,7 +17,7 @@ class Home5 extends StatefulWidget {
 
 class _Home5State extends State<Home5> {
   final _auth = FirebaseAuth.instance;
-  String userEmail,userImage,testU1,testU2,testU3;
+  String userEmail, userImage, testU1, testU2, testU3;
   Size size;
 
   @override
@@ -35,158 +34,167 @@ class _Home5State extends State<Home5> {
       print(user.email);
       setState(() {
         userEmail = user.email;
-        userImage =  user.photoURL;
-        testU1=user.displayName;
-        testU2=user.uid;
+        userImage = user.photoURL;
+        testU1 = user.displayName;
+        testU2 = user.uid;
       });
     } else {
       print('check2');
     }
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     size = MediaQuery.of(context).size;
     return Scaffold(
-
- body: Column(
-   children: [
-     Container(
-       height: size.height*0.1,
-
-       decoration: BoxDecoration(
-
-         color: Colors.teal,
-         borderRadius: BorderRadius.circular(20.0),
-
-       ),
-
-     ),
-     Container(
-       height: size.height*0.3,
-       child:Center(child: Column(
-         children: [
-           CircleAvatar(
-             backgroundImage: NetworkImage(userImage),
-           ),
-
-           Column(
-             children: [
-               Text(userEmail),
-               Text(testU1),
-
-             ],
-           ),
-           ElevatedButton(
-               onPressed: () async {
-                 await signOutGoogle();
-                 setState(() {
-                   userEmail = '';
-                 });
-
-
-               },
-               child: Text('Logout')),
-
-         ],
-       )),
-       decoration: BoxDecoration(
-
-         color: Colors.green.shade100,
-         borderRadius: BorderRadius.only(
-           bottomLeft: Radius.circular(36),
-           bottomRight: Radius.circular(36),
-         ),
-       ),
-     ),
-
-
-
-
-     Container(
-       height: size.height*0.4,
-       width: size.width,
-       child: Column(
-
-         children: [
-           Center(child: Text('Services')),
-
-           Row(
-             children: [
-               ReusableCard_2(colour: Colors.green,cardChild: Padding(
-                 padding: const EdgeInsets.all(10.0),
-                 
-                 child: Container(
-
-                     height: size.height*0.1,
-
-                     child: Text('Select Plant')),
-               ),onPress: (){ Navigator.pushNamed(context, SelectPlant.id);},),
-               ReusableCard_2(colour: kAquaHaze,cardChild: Text('dummy card'),),
-               ReusableCard_2(colour: kAquaHaze,cardChild: Text('dummy card'),),
-             ],
-           ),
-
-           Row(
-             children: [
-               ReusableCard_2(colour: kAquaHaze,cardChild: Text('dummy card'),),
-               ReusableCard_2(colour: kAquaHaze,cardChild: Text('dummy card'),),
-               ReusableCard_2(colour: kAquaHaze,cardChild: Text('dummy card'),),
-             ],
-           ),
-
-
-
-
-         ],
-       ),
-
-
-
-
-
-
-
-
-
-
-
-
-       decoration: BoxDecoration(
-
-         color: Colors.red,
-         borderRadius: BorderRadius.circular(20.0),
-
-       ),
-
-     ),
-     Container(
-       height: size.height*0.2,
-
-       decoration: BoxDecoration(
-
-         color: Colors.blue,
-         borderRadius: BorderRadius.circular(20.0),
-
-       ),
-
-     ),
-
-
-   ],
-
-
-
-
- ),
+      body: Column(
+        children: [
+          Container(
+            height: size.height * 0.1,
+            decoration: BoxDecoration(
+              color: Colors.teal,
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+          ),
+          Container(
+            height: size.height * 0.3,
+            child: Center(
+                child: Column(
+              children: [
+                CircleAvatar(
+                  backgroundImage: NetworkImage(userImage),
+                ),
+                Column(
+                  children: [
+                    Text(userEmail),
+                    Text(testU1),
+                  ],
+                ),
+                ElevatedButton(
+                    onPressed: () async {
+                      await signOutGoogle();
+                      setState(() {
+                        userEmail = '';
+                      });
+                    },
+                    child: Text('Logout')),
+              ],
+            )),
+            decoration: BoxDecoration(
+              color: Colors.green.shade100,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(36),
+                bottomRight: Radius.circular(36),
+              ),
+            ),
+          ),
+          Container(
+            height: size.height * 0.4,
+            width: size.width,
+            child: Column(
+              children: [
+                Center(child: Text('Services')),
+                Row(
+                  children: [
+                    ReusableCard_2(
+                      colour: Colors.green,
+                      cardChild: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                            height: size.height * 0.1,
+                            child: Text('Select Plant')),
+                      ),
+                      onPress: () {
+                        Navigator.pushNamed(context, SelectPlant.id);
+                      },
+                    ),
+                    ReusableCard_2(
+                      colour: Colors.green,
+                      cardChild: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                            height: size.height * 0.1,
+                            child: Text('Select Plant')),
+                      ),
+                      onPress: () {
+                        Navigator.pushNamed(context, SelectPlant.id);
+                      },
+                    ),
+                    ReusableCard_2(
+                      colour: Colors.green,
+                      cardChild: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                            height: size.height * 0.1,
+                            child: Text('Select Plant')),
+                      ),
+                      onPress: () {
+                        Navigator.pushNamed(context, SelectPlant.id);
+                      },
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    ReusableCard_2(
+                      colour: Colors.green,
+                      cardChild: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                            height: size.height * 0.1,
+                            child: Text('Select Plant')),
+                      ),
+                      onPress: () {
+                        Navigator.pushNamed(context, SelectPlant.id);
+                      },
+                    ),
+                    ReusableCard_2(
+                      colour: Colors.green,
+                      cardChild: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                            height: size.height * 0.1,
+                            child: Text('Select Plant')),
+                      ),
+                      onPress: () {
+                        Navigator.pushNamed(context, SelectPlant.id);
+                      },
+                    ),
+                    ReusableCard_2(
+                      colour: Colors.green,
+                      cardChild: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                            height: size.height * 0.1,
+                            child: Text('Select Plant')),
+                      ),
+                      onPress: () {
+                        Navigator.pushNamed(context, SelectPlant.id);
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+          ),
+          Container(
+            height: size.height * 0.2,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+          ),
+        ],
+      ),
     );
   }
+
   Future<void> signOutGoogle() async {
     await googleSignIn.signOut();
     Navigator.pushNamed(context, LoginWithGoogle.id);
   }
 }
-
-
